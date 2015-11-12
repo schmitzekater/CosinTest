e9de743c1064dfecb0ede9505653d2e9d37db108
schmitzekater <alexander@schmitzekater.de>
2015-11-12 20:30:33 +0100

    Register user angefangen

--- grails-app/domain/de/schmitzekater/Person.groovy
+++ grails-app/domain/de/schmitzekater/Person.groovy
@@ -3,6 +3,7 @@ package de.schmitzekater
 class Person {
     static hasOne = [user: User, department: Department]
     static hasMany = [system: System]
+    static auditable = true
     String firstName
     String lastName
     String email
@@ -19,5 +20,21 @@ class Person {
     String getDisplayString() {
         return lastName + ", " + firstName
     }
+    def onSave = {
+        println "new User $userId inserted"
+        // may optionally refer to newState map
+    }
+    def onDelete = {
+        println "User $userId was deleted"
+        // may optionally refer to oldState map
+    }
+    def onChange = { oldMap, newMap ->
+        println "User $userId was changed ..."
+        oldMap.each({ key, oldVal ->
+            if (oldVal != newMap[key]) {
+                println " * $key changed from $oldVal to " + newMap[key] + " for " + userId
+            }
+        })
+    }//*/
 
 }
