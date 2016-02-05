3d9f767976678e189f80eeb4d66c39d01063d069
schmitzekater <alexander@schmitzekater.de>
2016-02-05 11:08:20 +0100

    Report angepasst

--- grails-app/services/de/schmitzekater/SystemRoleService.groovy
+++ grails-app/services/de/schmitzekater/SystemRoleService.groovy
@@ -2,13 +2,20 @@ package de.schmitzekater
 
 import grails.transaction.Transactional
 
+/**
+ * @author Alexander Schmitz
+ *
+ * Service to create SystemRoles.
+ * One computer might have multiple Roles in a System, this is achieved with this service.
+ */
 @Transactional
 class SystemRoleService {
 
     def createSystemRole(Computer computer, System system, ComputerRole cr) {
         if(!SystemRole.exists(computer.id, system.id, cr.id))
         {
-            true
+            def systemRole = SystemRole.create(computer, system, cr, true)
+            if (systemRole) true
         }
         else{
             false
